de22044575b7daf4a3ef23715f1f919b12b59b07
YassineK22 <yassinekraiem22@gmail.com>
2022-11-22 19:01:27 +0100

    fix js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const SubEl = document.getElementById("btn");
-const thanksContainer = document.getElementById("thx-container")
-const valueRated = document.getElementById("value-rated")
+const thanksContainer = document.getElementById("thx-container");
+const valueRated = document.getElementById("value-rated");
 const R1 = document.getElementById("R1");
 const R2 = document.getElementById("R2");
 const R3 = document.getElementById("R3");
@@ -13,29 +13,29 @@ SubEl.addEventListener("click", ()=>{
     thanksContainer.classList.add("active")
 })
 
-R1.addEventListener("click", ()=>{
-    R1.classList.add("hoverC")
-    value = 1;
-})
+// R1.addEventListener("click", ()=>{
+//     R1.classList.add("hoverC")
+//     value = 1;
+// })
 
-R2.addEventListener("click", ()=>{
-    R2.classList.add("hoverC");
-    value = 2;
-})
+// R2.addEventListener("click", ()=>{
+//     R2.classList.add("hoverC");
+//     value = 2;
+// })
 
-R3.addEventListener("click", ()=>{
-    R3.classList.add("hoverC");
-    value = 3;
-})
+// R3.addEventListener("click", ()=>{
+//     R3.classList.add("hoverC");
+//     value = 3;
+// })
 
-R4.addEventListener("click", ()=>{
-    R4.classList.add("hoverC")
-    value = 4;
-})
+// R4.addEventListener("click", ()=>{
+//     R4.classList.add("hoverC")
+//     value = 4;
+// })
 
-R5.addEventListener("click", ()=>{
-    R5.classList.add("hoverC")
-    value = 5;
-})
+// R5.addEventListener("click", ()=>{
+//     R5.classList.add("hoverC")
+//     value = 5;
+// })
 
 valueRated.innerText = value;
