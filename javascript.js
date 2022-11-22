459f4f941c6e5d2318b0d9c2a116c71b7bc9f3d6
YassineK22 <78359891+YassineK22@users.noreply.github.com>
2022-11-22 19:35:26 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,41 +1,64 @@
-const SubEl = document.getElementById("btn");
-const thanksContainer = document.getElementById("thx-container");
-const valueRated = document.getElementById("value-rated");
+const SubEL = document.getElementById("btn");
+const thanksContainer = document.getElementById("thx-container")
+const howContainer = document.getElementById("how-container")
+const valueRated = document.getElementById("value-rated")
 const R1 = document.getElementById("R1");
 const R2 = document.getElementById("R2");
 const R3 = document.getElementById("R3");
 const R4 = document.getElementById("R4");
 const R5 = document.getElementById("R5");
 
-value = 0;
-
-SubEl.addEventListener("click", ()=>{
+value = "0";
+console.log("Clicked")
+SubEL.addEventListener("click", ()=>{
+    console.log("Clicked")
     thanksContainer.classList.add("active")
+    howContainer.classList.add("off")
 })
 
-// R1.addEventListener("click", ()=>{
-//     R1.classList.add("hoverC")
-//     value = 1;
-// })
+R1.addEventListener("click", ()=>{
+    R1.classList.add("hoverC")
+    R2.classList.remove("hoverC")
+    R3.classList.remove("hoverC")
+    R4.classList.remove("hoverC")
+    R5.classList.remove("hoverC")
+    valueRated.innerText = 1;
+})
 
-// R2.addEventListener("click", ()=>{
-//     R2.classList.add("hoverC");
-//     value = 2;
-// })
+R2.addEventListener("click", ()=>{
+    R1.classList.remove("hoverC")
+    R2.classList.add("hoverC")
+    R3.classList.remove("hoverC")
+    R4.classList.remove("hoverC")
+    R5.classList.remove("hoverC")
+    valueRated.innerText = 2;
+})
 
-// R3.addEventListener("click", ()=>{
-//     R3.classList.add("hoverC");
-//     value = 3;
-// })
+R3.addEventListener("click", ()=>{
+    R1.classList.remove("hoverC")
+    R2.classList.remove("hoverC")
+    R3.classList.add("hoverC")
+    R4.classList.remove("hoverC")
+    R5.classList.remove("hoverC")
+    valueRated.innerText = 3;
+})
 
-// R4.addEventListener("click", ()=>{
-//     R4.classList.add("hoverC")
-//     value = 4;
-// })
+R4.addEventListener("click", ()=>{
+    R1.classList.remove("hoverC")
+    R2.classList.remove("hoverC")
+    R3.classList.remove("hoverC")
+    R4.classList.add("hoverC")
+    R5.classList.remove("hoverC")
+    valueRated.innerText = 4;
+})
 
-// R5.addEventListener("click", ()=>{
-//     R5.classList.add("hoverC")
-//     value = 5;
-// })
+R5.addEventListener("click", ()=>{
+    R1.classList.remove("hoverC")
+    R2.classList.remove("hoverC")
+    R3.classList.remove("hoverC")
+    R4.classList.remove("hoverC")
+    R5.classList.add("hoverC")
+    valueRated.innerText = 5;
+})
 
 valueRated.innerText = value;
